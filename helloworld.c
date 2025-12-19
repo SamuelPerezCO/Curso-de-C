6bac4440a8ac1b12f8a2b5d41e0f471970bb7371
Samuel Pérez Serna <130716411+SamuelPerezCO@users.noreply.github.com>
2025-12-19 10:49:59 -0500

    Class 7

--- helloworld.c
+++ helloworld.c
@@ -6,4 +6,24 @@ int main()
     printf("Hello platzi");
     printf("Hello Student");
     return 0;
+
 }
+
+/*
+Basic Types:
+    Valores Enteros
+    (A) Aritmeticos int 2 o 4 Bytes -32,768 to 32,767 or -2,147,483,648 to 2,147,483,647
+                    unsigned int 0 to 65,535 or 0 to 4,294,967,295
+
+                    short 2 Bytes -32,768 to 32,767
+                    unsigned short 2 bytes 0 to 65,535
+
+                    long 8 bytes -9223372936854775808 to 9223372036854775807
+                    unsigned long 0 to 18446744073709551615
+
+
+    (B) De punto Flotante float
+                    float 4 bytes 1.2E-38 to 3.4E+38 6 decimal places
+                    double 8 bytes 2.3E-308 to 1.7E+308 15 decimal places
+                    long double 10 bytes 3.4E-4932 to 1.1E+4932 19 decimal places
+*/
